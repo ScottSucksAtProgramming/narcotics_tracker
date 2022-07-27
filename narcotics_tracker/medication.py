42aef926e887d15ebac899bdb6ff02c48c54c7a1
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-07-27 10:57:41 -0400

    Update formatting

--- narcotics_tracker/medication.py
+++ narcotics_tracker/medication.py
@@ -4,18 +4,30 @@
 class Medication:
     """Model of a medication."""
 
-    def __init__(self, name, manufacturer, ndc_number, container_type, box_quantity, fill_amount_in_milliliters, dosage_in_mg, dose_unit, concentration):
+    def __init__(
+        self,
+        name: str,
+        manufacturer: str,
+        ndc_number: str,
+        box_quantity: int,
+        container_type: str,
+        fill_amount_in_milliliters: float,
+        dosage_in_mg: float,
+        dose_unit: str,
+        concentration: float,
+    ):
         """Initialize a medication."""
         self.name = name
         self.manufacturer = manufacturer
         self.ndc_number = ndc_number
-        self.container_type = container_type
         self.box_quantity = box_quantity
+        self.container_type = container_type
         self.fill_amount_in_milliliters = fill_amount_in_milliliters
         self.dosage_in_mg = dosage_in_mg
         self.dose_unit = dose_unit
         self.concentration = concentration
 
 
-fentanyl = Medication("Fentanyl", "Umbrella Corp", "123456789",
-                      "Vial", 25, 2, 0.1, "mcg", 0.05)
+fentanyl = Medication(
+    "Fentanyl", "Umbrella Corp", "123456789", "Vial", 25, 2, 0.1, "mcg", 0.05
+)
