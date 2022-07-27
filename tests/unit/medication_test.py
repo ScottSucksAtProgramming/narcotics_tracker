7993919a0633b9d64f3b8e4298bcf5a21906247c
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-07-27 01:05:00 -0400

    Add 3 more tests to medication_test.py

--- tests/unit/medication_test.py
+++ tests/unit/medication_test.py
@@ -4,5 +4,26 @@ from narcotics_tracker.medication import Medication
 class TestMedication:
 
     def test_can_see_Medication_class(self):
-        """Can we see the Medications class?"""
+        """Checks to see if Medication class is defined."""
         assert type(Medication) == type(object)
+
+    def test_can_instantiate_Medication_object(self):
+        """Check to see if Medication object can be instantiated."""
+
+        fentanyl = Medication("Fentanyl", "Umbrella Corp", "123456789",
+                              "Vial", 25, 2, 0.1, "mcg", 0.05)
+        assert isinstance(fentanyl, Medication)
+
+    def test_can_get_name(self):
+        """Check to see if name can be retrieved."""
+
+        fentanyl = Medication("Fentanyl", "Umbrella Corp", "123456789",
+                              "Vial", 25, 2, 0.1, "mcg", 0.05)
+        assert fentanyl.name == "Fentanyl"
+
+    def test_can_get_dosage_in_mg(self):
+        """Check to see if dosage_in_mg can be retrieved."""
+
+        fentanyl = Medication("Fentanyl", "Umbrella Corp", "123456789",
+                              "Vial", 25, 2, 0.1, "mcg", 0.05)
+        assert fentanyl.dosage_in_mg == 0.1
