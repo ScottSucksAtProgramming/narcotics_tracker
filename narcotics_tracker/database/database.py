ee2143fa3fc7c8776e97174eb590dc7e1e223cbd
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-08-09 00:06:35 -0400

    Add docstrings to database.py

--- narcotics_tracker/database/database.py
+++ narcotics_tracker/database/database.py
@@ -7,17 +7,26 @@ class Database:
     """Reads and writes to the inventory database."""
 
     def __init__(self):
-        """Initializes the database connection."""
+        """Initializes the database object and sets connection to None."""
         self.database_connection = None
 
     def connect(self, database_file):
-        """Connects to the database."""
+        """Connects to the database file.
+
+        Args:
+            database_file (str): The name of the database file. (File must be
+                in the data/ directory.)"""
         self.database_connection = sqlite3.connect(
             "narcotics_tracker/data/" + database_file
         )
 
     def create_table(self, sql_query):
-        """Writes to the database."""
+        """Creates a table in the database.
+
+        Args:
+            sql_query (str): The SQL query to create the table. i.e.
+                "CREATE TABLE table_name (column_name column_type)"
+        """
         cursor = self.database_connection.cursor()
         cursor.execute(sql_query)
 
