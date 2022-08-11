98ebe2d520ea0c3687c5bc5b25c82046bf271b16
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-08-11 01:12:58 -0400

    Remove unneeded files after restructuring

--- narcotics_tracker/date/date.py
+++
@@ -1,9 +0,0 @@
-"""Contains functions to get the date."""
-
-import datetime
-
-
-def get_date_as_string():
-    """Returns the date in the format DD-MM-YYYY"""
-
-    return datetime.date.today().strftime("%m-%d-%Y")
