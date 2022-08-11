7bdac327e6d60d5c9d295e72ff11ae109c98e155
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-08-11 01:15:30 -0400

    Update for restructuring

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,23 @@
 """Configuration for pytest."""
 
 from pytest import fixture
+from narcotics_tracker.enums import containers, medication_statuses
 
-from narcotics_tracker.units import units
-from narcotics_tracker.medication import (
-    builder,
-    containers,
-    medication_status,
-    medication,
-)
+from narcotics_tracker.enums import units
+from narcotics_tracker.builders import builder
 
 
 @fixture
 def test_med():
     """Return a Medication object for testing."""
 
-    medication_builder = builder.MedicationBuilder()
+    medication_builder = builder.ObjectBuilder()
     medication_builder.set_name("Unobtanium")
     medication_builder.set_code("Un-69420-9001")
-    medication_builder.set_container_type(containers.Container.VIAL)
+    medication_builder.set_container(containers.Container.VIAL)
     medication_builder.set_dose_and_unit(69_420, units.Unit.MCG)
     medication_builder.set_fill_amount(9_001)
-    medication_builder.set_status(medication_status.MedicationStatus.DISCONTINUED)
+    medication_builder.set_status(medication_statuses.MedicationStatus.DISCONTINUED)
 
     test_med = medication_builder.build
     test_med.medication_id = 1
