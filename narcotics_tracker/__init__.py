83460cd7050d43fdd5fe0931539424ee17f6d0a4
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-08-16 11:51:41 -0400

    Update docstring for the Narcotics Tracker

---
+++ narcotics_tracker/__init__.py
@@ -0,0 +1,54 @@
+"""Tracks the inventory of controlled substances used at EMS agencies.
+
+Version: 0.1.0
+
+Author: Scott Kostolni - (https://github.com/ScottSucksAtProgramming)
+
+Special Thanks:
+    Mom, thanks for being my software design mentor and entertaining my 
+    onslaught of questions.
+
+Purpose:
+    Welcome to the Narcotics Tracker! This software is designed to assist 
+    controlled substance agents working at EMS agencies with inventory 
+    tracking and periodic reporting of controlled substance activities to 
+    their governing agencies.
+
+Motivation:
+    I am a controlled substance agent working in New York State. The tracking 
+    of controlled substances is complicated and requires multiple forms and 
+    processes. I have wished for a single place where all of that information 
+    can be stored and easily accessed. This software is intended to fill that 
+    need.
+
+Current Completed Features:
+
+    Medication Module - Creates and stores medication objects in the database.
+
+    Database Module - Handles communication with the SQLite3 database.
+
+    Builders Package - Assists with the building of complex objects.
+
+    Enums Package - Contains the enumerated types used in the software.
+
+    Utils Package - Contains utility functions used in the software.
+
+Planned Features:
+
+    Inventory Tracking (v0.2.0)
+
+    Lot Tracking (v0.3.0)
+
+    Order Tracking (v0.4.0)
+
+    Destruction Tracking (v0.5.0)
+
+    User and Agent Management (v0.6.0)
+
+    Report Generation (v0.7.0)
+
+    Console Interface (v0.8.0)
+
+    GUI Interface (v1.0.0)
+
+"""
