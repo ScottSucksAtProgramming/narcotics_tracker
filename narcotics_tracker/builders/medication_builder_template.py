a1d5debae9b188c8007748ce69902a950b8be0ee
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-08-17 00:32:37 -0400

    Fix builder issues

---
+++ narcotics_tracker/builders/medication_builder_template.py
@@ -0,0 +1,57 @@
+"""Contains the templates for the builder classes."""
+
+
+from abc import ABC, abstractmethod
+
+
+class Medication(ABC):
+    """The MedicationBuilder interface defines methods to build a medication."""
+
+    @abstractmethod
+    def set_medication_id(self) -> None:
+        pass
+
+    @abstractmethod
+    def set_name(self) -> None:
+        pass
+
+    @abstractmethod
+    def set_code(self) -> None:
+        pass
+
+    @abstractmethod
+    def set_container(self) -> None:
+        pass
+
+    @abstractmethod
+    def set_fill_amount(self) -> None:
+        pass
+
+    @abstractmethod
+    def set_dose_and_unit(self) -> None:
+        pass
+
+    @abstractmethod
+    def set_concentration(self) -> None:
+        pass
+
+    @abstractmethod
+    def set_status(self) -> None:
+        pass
+
+    @abstractmethod
+    def set_created_date(self) -> None:
+        pass
+
+    @abstractmethod
+    def set_modified_date(self) -> None:
+        pass
+
+    @abstractmethod
+    def set_modified_by(self) -> None:
+        pass
+
+    @property
+    @abstractmethod
+    def build(self) -> None:
+        pass
