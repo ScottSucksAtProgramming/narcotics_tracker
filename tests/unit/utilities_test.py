ab79c8e9e3751ce0e7291cba6dbcb88ce7a73957
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-08-23 19:30:06 -0400

    Update docstring for utilities_test module

--- tests/unit/utilities_test.py
+++ tests/unit/utilities_test.py
@@ -1,67 +1,95 @@
-"""Contains the unit tests for the utilities module."""
+"""Contains the classes used to unit tests the utils package.
+
+Classes:
+    Test_UnitConverter: Contains all unit tests for the unit_converter module.
+    Test_Utilities: Contains all unit tests for the utilities module.
+
+"""
 
 from narcotics_tracker.enums import containers, medication_statuses, units
 from narcotics_tracker.utils import unit_converter, utilities
 
 
-class Test_UnitConverterClass:
-    """Unit tests for the UnitConverter class."""
+class Test_UnitConverter:
+    """Contains all unit tests for the unit_converter module.
+
+    Behaviors Tested:
+        - Can convert mg to mcg.
+        - Can convert mg to G.
+        - Can convert G to mcg.
+        - Can convert G to mg.
+        - Can convert mcg to mg.
+        - Can convert mcg to G.
+    """
 
     def test_convert_mg_to_mcg(self):
-        """Check to see if mg to mcg conversion works."""
+        """Check to see if mg can be converted to mcg.
 
+        Asserts that 5 mg returns 5_000 mcg.
+        """
         assert unit_converter.UnitConverter.to_mcg(5, "mg") == 5_000
 
     def test_convert_G_to_mcg(self):
-        """
-        Test that G is converted to mcg.
-        """
+        """Check to see if G can be converted to mcg.
 
+        Asserts that 0.9 G returns 900_000 mcg.
+        """
         assert unit_converter.UnitConverter.to_mcg(0.9, "G") == 900_000
 
     def test_convert_mcg_to_mg(self):
-        """
-        Test that mcg is converted to mg.
-        """
+        """Check to see if mcg can be converted to mg.
 
+        Asserts that 500 mcg returns 0.5 mg.
+        """
         assert unit_converter.UnitConverter.to_mg(500, "mcg") == 0.5
 
     def test_convert_G_to_mg(self):
-        """
-        Test that G is converted to mg.
-        """
+        """Check to see if G can be converted to mg.
 
+        Asserts that 0.9 G returns 900 mg.
+        """
         assert unit_converter.UnitConverter.to_mg(0.9, "G") == 900
 
     def test_convert_mcg_to_G(self):
-        """
-        Test that mcg is converted to G.
-        """
+        """Check to see if mcg can be converted to G.
 
+        Asserts that 1 mcg returns 0.000001 G.
+        """
         assert unit_converter.UnitConverter.to_G(1, "mcg") == 0.000001
 
-    def test_convert_mg_to_D(self):
+    def test_convert_mg_to_G(self):
         """
         Test that mg is converted to G.
         """
-
         assert unit_converter.UnitConverter.to_G(5, "mg") == 0.005
 
 
-class Test_UtilitiesClass:
-    """Unit tests for the Utilities class."""
+class Test_Utilities:
+    """Contains all unit tests for the utilities module.
+
+    Behaviors Tested:
+        - Can create Container enum from string.
+        - Can create Unit enum from string.
+        - Can create MedicationsStatus enum from string.
+    """
 
     def test_from_string_for_Container(self):
-        """Check that from_string returns the correct enum value."""
+        """Tests enum_from_string creates a Container from its string value.
 
+        Asserts that 'Pre-filled Syringe' equals
+        'containers.Container.PRE_FILLED_SYRINGE'.
+        """
         assert (
             utilities.enum_from_string(containers.Container, "Pre-filled Syringe")
             == containers.Container.PRE_FILLED_SYRINGE
         )
 
     def test_from_string_for_MedicationStatus(self):
-        """Check that from_string returns the correct enum value."""
+        """Tests enum_from_string creates a MedicationStatus from its value.
 
+        Asserts that 'disConTinueD' equals
+        'medication_statuses.MedicationStatus.Discontinued'.
+        """
         assert (
             utilities.enum_from_string(
                 medication_statuses.MedicationStatus, "disConTinueD"
@@ -70,6 +98,8 @@ class Test_UtilitiesClass:
         )
 
     def test_from_string_for_Unit(self):
-        """Check that from_string returns the correct enum value."""
+        """Tests enum_from_string creates a Unit from its string value.
 
+        Asserts that 'mg' equals 'units.Unit.MG'.
+        """
         assert utilities.enum_from_string(units.Unit, "mg") == units.Unit.MG
