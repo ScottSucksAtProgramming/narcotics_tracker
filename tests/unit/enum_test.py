0220d5c1243bd374b5e3e1732569651986bff04a
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-08-23 12:11:25 -0400

    Update docstring for enum_test.py

--- tests/unit/enum_test.py
+++ tests/unit/enum_test.py
@@ -1,75 +1,159 @@
-"""Contains all unit tests for the enums used in the narcotics_tracker."""
+"""Contains the Test_Container, Test_MedicationStatus and Test_Unit classes.
 
+Classes:
+    Test_Container: Contains all unit tests for the containers module.
+    
+    Test_MedicationStatus: Contains all unit tests for the medication_status module."
+    
+    Test_Unit: Contains all unit tests for the units module.
+    """
 import pytest
 
 from narcotics_tracker import medication
 from narcotics_tracker.enums import containers, medication_statuses, units
 
 
-class Test_ContainerClass:
-    """Unit tests for the Container class."""
+class Test_Container:
+    """Contains all unit tests for the containers module.
+
+    Behaviors Tested:
+        - Tests that the Enum values return the correct string.
+        - Tests that an invalid value raises an 'AttributeError'.
+    """
 
     def test_VIAL_returns_correct_string(self):
-        """Check that VIAL returns the correct string."""
+        """Tests that VIAL returns the correct string.
 
+        Asserts that value equals 'Vial'.
+        """
         assert containers.Container.VIAL.value == "Vial"
 
     def test_AMPULE_returns_correct_string(self):
-        """Check that AMPULE returns the correct string."""
+        """Tests that AMPULE returns the correct string.
 
+        Asserts that value equals 'Ampule'.
+        """
         assert containers.Container.AMPULE.value == "Ampule"
 
     def test_PRE_FILLED_SYRINGE_returns_correct_string(self):
-        """Check that PRE_FILLED_SYRINGE returns the correct string."""
+        """Tests that PRE_FILLED_SYRINGE returns the correct string.
 
+        Asserts that value equals 'Pre-filled Syringe'.
+        """
         assert containers.Container.PRE_FILLED_SYRINGE.value == "Pre-filled Syringe"
 
     def test_PRE_MIXED_BAG_returns_correct_string(self):
-        """Check that PRE_MIXED_BAG returns the correct string."""
+        """Tests that PRE_MIXED_BAG returns the correct string.
 
+        Asserts that value equals 'Pre-mixed Bag'.
+        """
         assert containers.Container.PRE_MIXED_BAG.value == "Pre-mixed Bag"
 
+    def test_can_restrict_status_to_Container_enum(self):
+        """Tests that incorrect containers raise an exception.
 
-class Test_MedicationStatusClass:
-    """Unit tests for the MedicationStatus class."""
+        Test passes if an 'AttributeError' is raised.
+        """
+        with pytest.raises(AttributeError):
+            fentanyl = medication.Medication(
+                name="Fentanyl",
+                code="Fe-100-2",
+                container_type=containers.Container.BOX,
+                fill_amount=2,
+                dose=100,
+                unit=units.Unit.MCG,
+                concentration=50,
+                status=medication_statuses.MedicationStatus.ACTIVE,
+                created_date="08-01-2022",
+                modified_date="08-01-2022",
+                modified_by="test",
+            )
+
+
+class Test_MedicationStatus:
+    """Contains all unit tests for the medication_status module.
+
+    Behaviors Tested:
+        - Tests that the Enum values return the correct string.
+        - Tests that an invalid value raises an 'AttributeError'.
+    """
 
     def test_ACTIVE_returns_correct_string(self):
-        """Check that ACTIVE returns the correct string."""
+        """Tests that ACTIVE returns the correct string.
 
+        Asserts that value equals 'Active'.
+        """
         assert medication_statuses.MedicationStatus.ACTIVE.value == "Active"
 
     def test_INACTIVE_returns_correct_string(self):
-        """Check that INACTIVE returns the correct string."""
+        """Tests that INACTIVE returns the correct string.
 
+        Asserts that value equals 'Inactive'.
+        """
         assert medication_statuses.MedicationStatus.INACTIVE.value == "Inactive"
 
     def test_DISCONTINUED_returns_correct_string(self):
-        """Check that DISCONTINUED returns the correct string."""
+        """Tests that DISCONTINUED returns the correct string.
 
+        Asserts that value equals 'Discontinued'.
+        """
         assert medication_statuses.MedicationStatus.DISCONTINUED.value == "Discontinued"
 
+    def test_can_restrict_status_to_MedicationStatus_enum(self):
+        """Tests that incorrect statuses raise an exception.
 
-class Test_UnitClass:
-    """Unit tests for the Unit class."""
+        Test passes if an 'AttributeError' is raised.
+        """
+        with pytest.raises(AttributeError):
+            fentanyl = medication.Medication(
+                name="Fentanyl",
+                code="Fe-100-2",
+                container_type=containers.Container.VIAL,
+                fill_amount=2,
+                dose=100,
+                unit=units.Unit.MCG,
+                concentration=50,
+                status=medication_statuses.MedicationStatus.ON_VACATION,
+                created_date="08-01-2022",
+                modified_date="08-01-2022",
+                modified_by="test",
+            )
+
+
+class Test_Unit:
+    """Contains all unit tests for the units module.
+
+    Behaviors Tested:
+        - Tests that the Enum values return the correct string.
+        - Tests that an invalid value raises an 'AttributeError'.
+    """
 
     def test_MCG_returns_correct_string(self):
-        """Check that MCG returns the correct string."""
+        """Tests that MCG returns the correct string.
 
+        Asserts that value equals 'mcg'.
+        """
         assert units.Unit.MCG.value == "mcg"
 
     def test_MG_returns_correct_string(self):
-        """Check that MG returns the correct string."""
+        """Tests that MG returns the correct string.
 
+        Asserts that value equals 'mg'.
+        """
         assert units.Unit.MG.value == "mg"
 
     def test_G_returns_correct_string(self):
-        """Check that G returns the correct string."""
+        """Tests that G returns the correct string.
 
+        Asserts that value equals 'G'.
+        """
         assert units.Unit.G.value == "G"
 
     def test_can_restrict_unit_to_Unit_enum(self):
-        """Check that incorrect units raise an exception."""
+        """Tests that incorrect units raise an exception.
 
+        Test passes if an 'AttributeError' is raised.
+        """
         with pytest.raises(AttributeError):
             fentanyl = medication.Medication(
                 name="Fentanyl",
