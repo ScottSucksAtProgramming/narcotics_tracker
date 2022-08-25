3e095f69caace61509b7bf4af87bf7cd47003f0a
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-08-25 19:47:33 -0400

    Update setup.py to create reporting_periods table

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,13 +1,23 @@
 """This module will act as a setup script for the narcotics_tracker."""
 
-from narcotics_tracker import medication
+import sqlite3
+
+from narcotics_tracker import database, medication, periods
 
 
-def create_medication_table(db_connection) -> None:
+def create_medication_table(db_connection: sqlite3.Connection) -> None:
     """This function will create the medication table."""
-
     db_connection.create_table(medication.return_table_creation_query())
 
 
+def create_reporting_periods_table(db_connection: sqlite3.Connection) -> None:
+    """Creates the reporting_periods table."""
+    db_connection.create_table(periods.return_table_creation_query())
+
+
 if __name__ == "__main__":
-    create_medication_table()
+    db = database.Database()
+    db.connect("inventory.db")
+
+    create_medication_table(db)
+    create_reporting_periods_table(db)
