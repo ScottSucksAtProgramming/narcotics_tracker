cd923164086c981693e3683a708846ccaed737b0
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-09-04 19:49:07 -0400

    Remove Containers Enum

--- narcotics_tracker/enums/__init__.py
+++ narcotics_tracker/enums/__init__.py
@@ -3,7 +3,6 @@
 Tests for the enums package are located in the tests/unit/enum_test.py
 
 Modules:
-    containers: Enum for the container types.
 
     medication_statuses: Enum for the medication statuses.
 """
--- tests/unit/enum_test.py
+++ tests/unit/enum_test.py
@@ -10,64 +10,7 @@ Classes:
 import pytest
 
 from narcotics_tracker import medication
-from narcotics_tracker.enums import containers, medication_statuses
-
-
-class Test_Container:
-    """Contains all unit tests for the containers module.
-
-    Behaviors Tested:
-        - Tests that the Enum values return the correct string.
-        - Tests that an invalid value raises an 'AttributeError'.
-    """
-
-    def test_VIAL_returns_correct_string(self):
-        """Tests that VIAL returns the correct string.
-
-        Asserts that value equals 'Vial'.
-        """
-        assert containers.Container.VIAL.value == "Vial"
-
-    def test_AMPULE_returns_correct_string(self):
-        """Tests that AMPULE returns the correct string.
-
-        Asserts that value equals 'Ampule'.
-        """
-        assert containers.Container.AMPULE.value == "Ampule"
-
-    def test_PRE_FILLED_SYRINGE_returns_correct_string(self):
-        """Tests that PRE_FILLED_SYRINGE returns the correct string.
-
-        Asserts that value equals 'Pre-filled Syringe'.
-        """
-        assert containers.Container.PRE_FILLED_SYRINGE.value == "Pre-filled Syringe"
-
-    def test_PRE_MIXED_BAG_returns_correct_string(self):
-        """Tests that PRE_MIXED_BAG returns the correct string.
-
-        Asserts that value equals 'Pre-mixed Bag'.
-        """
-        assert containers.Container.PRE_MIXED_BAG.value == "Pre-mixed Bag"
-
-    def test_can_restrict_status_to_Container_enum(self):
-        """Tests that incorrect containers raise an exception.
-
-        Test passes if an 'AttributeError' is raised.
-        """
-        with pytest.raises(AttributeError):
-            fentanyl = medication.Medication(
-                name="Fentanyl",
-                code="Fe-100-2",
-                container_type=containers.Container.BOX,
-                fill_amount=2,
-                dose=100,
-                unit="mcg",
-                concentration=50,
-                status=medication_statuses.MedicationStatus.ACTIVE,
-                created_date="08-01-2022",
-                modified_date="08-01-2022",
-                modified_by="test",
-            )
+from narcotics_tracker.enums import medication_statuses
 
 
 class Test_MedicationStatus:
@@ -108,7 +51,7 @@ class Test_MedicationStatus:
             fentanyl = medication.Medication(
                 name="Fentanyl",
                 code="Fe-100-2",
-                container_type=containers.Container.VIAL,
+                container_type="Vial",
                 fill_amount=2,
                 dose=100,
                 unit="mcg",
