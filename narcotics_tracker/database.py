60a92733914f1a19788ddd64827a8454d6a1dcb3
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-10-14 19:08:55 -0400

    Add delete and select methods. Update docstrings.

--- narcotics_tracker/database.py
+++ narcotics_tracker/database.py
@@ -19,9 +19,14 @@ class DatabaseManager:
     """Sends SQL statements to the database, returns a cursor with results.
 
     Methods:
-        create_table: Creates a table in the database using the name and
-            dictionary.
-        add: Adds a row to the specified table using data from the dictionary.
+        create_table: Adds a table to the database using the given name and
+            column info.
+        add: Adds a row to the given table using the given data.
+        delete: Deletes a row from the given table using the given criteria.
+        select: Returns a cursor containing data for the given table and
+            criteria.
+
+
     """
 
     def __init__(self, filename: str) -> None:
@@ -50,10 +55,10 @@ class DatabaseManager:
 
         return cursor
 
-    def create_table(self, table_name: str, columns: dict[str]):
-        """Creates a table in the database using the name and dictionary.
+    def create_table(self, table_name: str, column_info: dict[str]):
+        """Adds a table to the database using the given name and column info.
 
-        This method will do nothing if the table already exists.
+        Does nothing if the table already exists.
 
         Args:
             table_name (str): The name of the table.
@@ -73,7 +78,7 @@ class DatabaseManager:
         self._execute(sql_statement)
 
     def add(self, table_name: str, data: dict[str]):
-        """Adds a row to the specified table using data from the dictionary.
+        """Adds a row to the given table using the given data.
 
         Args:
             table_name (str): The name of the table.
@@ -88,3 +93,47 @@ class DatabaseManager:
         column_values = tuple(data.values())
 
         self._execute(sql_statement, column_values)
+
+    def delete(self, table_name: str, criteria: dict[str]):
+        """Deletes a row from the given table using the given criteria.
+
+        Args:
+            table_name (str): The name of the table.
+            criteria (dict[str]): A dictionary mapping column names to values
+                used to select rows for deletion.
+        """
+        placeholders = [f"{column} = ?" for column in criteria.keys()]
+        criteria_columns = " AND ".join(placeholders)
+
+        sql_statement = f"""DELETE FROM {table_name} WHERE {criteria_columns};"""
+
+        criteria_values = tuple(criteria.values())
+
+        self._execute(sql_statement, criteria_values)
+
+    def select(
+        self, table_name: str, criteria: dict[str] = {}, order_by: str = None
+    ) -> sqlite3.Cursor:
+        """Returns a cursor containing data for the given table and criteria.
+
+        Args:
+            table_name (str): The name of the table.
+            criteria (dict[str]): A dictionary mapping column names to values
+                used to select rows from which to pull the data. Optional.
+            order_by (str): The name of the column by which to order the data.
+                Optional.
+
+        Returns:
+            sqlite3.Cursor: A cursor contains the returned data.
+        """
+        sql_query = f"""SELECT * FROM {table_name}"""
+
+        if criteria:
+            placeholders = [f"{column} = ?" for column in criteria.keys()]
+            criteria_columns = " AND ".join(placeholders)
+            sql_query += f" WHERE {criteria_columns}"
+
+        if order_by:
+            sql_query += f" ORDER BY {order_by}"
+
+        return self._execute(sql_query, tuple(criteria.values()))
