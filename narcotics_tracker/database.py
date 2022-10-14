9165a2c6bd9cfb2fb2ccd6e49dbe2287b4ce854d
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-10-14 18:46:27 -0400

    Add create_table and add methods

--- narcotics_tracker/database.py
+++ narcotics_tracker/database.py
@@ -16,7 +16,13 @@ import sqlite3
 
 
 class DatabaseManager:
-    """Sends SQL statements to the database, returns a cursor with results."""
+    """Sends SQL statements to the database, returns a cursor with results.
+
+    Methods:
+        create_table: Creates a table in the database using the name and
+            dictionary.
+        add: Adds a row to the specified table using data from the dictionary.
+    """
 
     def __init__(self, filename: str) -> None:
         """Initialize the DatabaseManager and stores the database file.
@@ -43,3 +49,42 @@ class DatabaseManager:
         cursor.execute(sql_statement, values or [])
 
         return cursor
+
+    def create_table(self, table_name: str, columns: dict[str]):
+        """Creates a table in the database using the name and dictionary.
+
+        This method will do nothing if the table already exists.
+
+        Args:
+            table_name (str): The name of the table.
+            columns (dict[str]): A dictionary mapping column names to its
+                datatype and restraints.
+        """
+        columns_with_details = [
+            f" {column_name} {details}" for column_name, details in columns.items()
+        ]
+
+        columns_with_details = ", ".join(columns_with_details)
+
+        sql_statement = (
+            f"""CREATE TABLE IF NOT EXISTS {table_name} ({columns_with_details});"""
+        )
+
+        self._execute(sql_statement)
+
+    def add(self, table_name: str, data: dict[str]):
+        """Adds a row to the specified table using data from the dictionary.
+
+        Args:
+            table_name (str): The name of the table.
+            data (dict[str]): A dictionary mapping column names to the values.
+        """
+        placeholders = ", ".join("?" for key in data.keys())
+        column_names = ", ".join(data.keys())
+        sql_statement = (
+            f"""INSERT INTO {table_name} ({column_names}) VALUES ({placeholders});"""
+        )
+
+        column_values = tuple(data.values())
+
+        self._execute(sql_statement, column_values)
