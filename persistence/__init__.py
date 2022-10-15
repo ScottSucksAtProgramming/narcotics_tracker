737f9e61fa27bd93defb4ccf8ca5dff503769f1b
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-10-14 20:23:01 -0400

    Add the Persistence Package

---
+++ persistence/__init__.py
@@ -0,0 +1,9 @@
+"""Contains modules which permanently store Narcotics Tracker data.
+
+This package serves as the persistence layer for this project.
+
+Modules:
+    Database (database.py): Manages Communication with the SQlite3 database.
+    Date Manager: (date_manager.py): Obtains and formats datetimes for the 
+        SQLite3 database.
+"""
