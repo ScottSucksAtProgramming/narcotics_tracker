02e54f0dc2d728edf4168be7c3fd1059dc6f6c0f
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-10-16 11:47:53 -0400

    Adds the SQLite Command module

--- narcotics_tracker/persistence/__init__.py
+++ narcotics_tracker/persistence/__init__.py
@@ -6,4 +6,6 @@ Modules:
     Database (database.py): Manages Communication with the SQlite3 database.
     Date Manager: (date_manager.py): Obtains and formats datetimes for the 
         SQLite3 database.
+    SQLite_Command (sqlite_command.py): Contains the protocol for any commands 
+        which affect the SQLite3 database.
 """
