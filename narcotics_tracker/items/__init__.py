579eea51c4a5347abed91b1140b31ef239e3a5fd
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-10-16 10:36:09 -0400

    Add Items Package

---
+++ narcotics_tracker/items/__init__.py
@@ -0,0 +1,12 @@
+"""Contains the items which are stored in the database.
+
+Modules:
+
+    Adjustments: 
+    Containers: 
+    Events: 
+    Medications: 
+    Reporting_Periods: 
+    Statuses: 
+    Units: 
+"""
