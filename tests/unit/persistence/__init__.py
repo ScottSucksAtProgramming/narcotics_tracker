51dc763c38878657e95e8b0f67fe0c6c83cf9684
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-10-17 16:22:12 -0400

    Remove Persistence package and Date Manager

--- tests/unit/persistence/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Contains all unit tests for the Persistence Package.
-
-Modules:
-    Database Test Module: Tests the Database module.
-    Date Manager Test Module: Tests the Date Manager module.
-    """
