2716e4bfba11798d1c20eba0ffd6cd90a36635db
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-10-17 09:05:28 -0400

    Add SaveEvent Command

--- narcotics_tracker/persistence/commands.py
+++ narcotics_tracker/persistence/commands.py
@@ -3,6 +3,7 @@
 Classes:
 
 """
+import time
 
 from narcotics_tracker.persistence.database import SQLiteManager
 from narcotics_tracker.persistence.sqlite_command import SQLiteCommand
@@ -11,7 +12,7 @@ from narcotics_tracker.persistence.sqlite_command import SQLiteCommand
 class SaveAdjustment(SQLiteCommand):
     """Adds an adjustment to the inventory table of the SQLite3 database."""
 
-    def __init__(self, receiver: SQLiteManager, adjustment_data: dict[str]) -> None:
+    def __init__(self, receiver: SQLiteManager) -> None:
         """Initializes the SaveAdjustment Command.
 
         Args:
@@ -25,3 +26,27 @@ class SaveAdjustment(SQLiteCommand):
     def execute(self, adjustment_data: dict[str]) -> None:
         """Executes the command."""
         self.target.add(table_name=self.table_name, data=adjustment_data)
+
+    class SaveEvent(SQLiteCommand):
+        """Adds an Event to the Events Table of the SQLite3 database."""
+
+        def __init__(self, receiver: SQLiteManager) -> None:
+            """Initializes the command and pairs it with a receiver instance.
+
+            Args:
+                receiver (SQLiteManager): The target of the command.
+            """
+            self.table_name = "events"
+            self.target = receiver
+
+        def execute(self, event_data: dict[str]) -> str:
+            """Executes the command, returns a message if successful.
+
+            Args:
+                event_data (dict[str]): A dictionary of event data mapping
+                    column names to values.
+            """
+            current_timestamp = int(time.time())
+            event_data["created_date"] = current_timestamp
+            event_data["modified_date"] = current_timestamp
+            self.target.add(self.table_name, event_data)
