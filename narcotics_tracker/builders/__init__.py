2e31ee8156eca09868cdda22b0e540c86236ee96
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-10-18 18:55:27 -0400

    Update documentation

--- narcotics_tracker/builders/__init__.py
+++ narcotics_tracker/builders/__init__.py
@@ -1,93 +1,59 @@
-"""Assists with building complex objects for the narcotics tracker.
+"""Contains the builders for the DataItems used in the Narcotics Tracker.
 
-Many objects in the narcotics tracker are complex and have a large number of 
-attributes. The builder design pattern was used to separate the creation of 
-the objects from their representations allowing objects to be created in a 
-modular fashion.
+#* Situation
 
-The builder package contains two types of builders: Abstract builders, which 
-serve as templates for the Concrete builders. The Concrete builders implement 
-the building process. Abstract builders are postfixed with the word 
-`template`.
+    There are multiple DataItems in the Narcotics Tracker. Each one stores 
+    various bits of information which enable inventory tracking. Each DataItem 
+    also inherits from the DataItem class which adds more attributes. This 
+    makes these items difficult to build with large constructors.
 
-Tests for the builders' package are located in the tests/unit/builders_test.py
+#* Background
 
-Modules:
+    This Package implements the Builder Pattern which helps solve this problem 
+    by separating the creation of these object from the objects themselves. 
+    Instead of using a DataItem's initializer a Builder has been created which 
+    constructs the object in a stepwise fashion. Each Builder implements the 
+    interface defined in a DataItemBuilder superclass.
 
-    Abstract Builder Templates:
+#* Intended Use
 
-        adjustment_builder_template: Contains the template for the Adjustment 
-            Builder.
-        
-        container_builder_template: Contains the template for the Container 
-            Builder.
-        
-        event_builder_template: Contains the template for the Event Builder.
-        
-        medication_builder_template: Contains the template for the Medication 
-            Builder.
-        
-        reporting_period_builder_template: Contains the template for the 
-            Reporting Period Builder.
-        
-        status_builder_template: Contains the template for the Status Builder.
-        
-        unit_builder_template: Contains the template for the Unit Builder.
+    When creating a DataItem, the builder for that item can be created and its 
+    methods can be called to build the object piece by piece. When all pieces 
+    have been constructed the `build` method will create return the object. 
+    After calling the build method, the builder is reset with a fresh instance 
+    of the DataItem object so it can be re-used if necessary.
 
-    Concrete Builder Modules:
+    #* Example Usage:
 
-        adjustment_builder: Contains the concrete builder for the Adjustment 
-            class.
-        
-        container_builder: Contains the concrete builder for the Container 
-            class.
-        
-        event_builder: Contains the concrete builder for the Event class.
-        
-        medication_builder: Contains the concrete builder for the Medication 
-            class.
-        
-        reporting_period_builder: ‌Contains the concrete builder for the 
-            ReportingPeriod class
-        
-        status_builder: Contains the concrete builder for the Status class.
-        
-        unit_builder: Contains the concrete builder for the Unit class.
+        ```python
 
+        med_builder = MedicationBuilder()
+        med_builder.set_table("medications")
+        med_builder.set_id()
+        med_builder.set_created_date()
+        med_builder.set_modified_date()
+        med_builder.set_modified_by("Narcotics Administrator")
+        med_builder.set_medication_code("fentanyl")
+        med_builder.set_medication_name("Fentanyl")
+        med_builder.set_fill_amount(2)
+        med_builder.set_medication_amount(100)
+        med_builder.set_preferred_unit("mcg")
+        med_builder.set_concentration()
+        med_builder.set_status("ACTIVE")
 
-How to Use the Builders:
-    
-    1. Import the builder for the object you want to build.
-    
-    2. If needed make a connection with the database using the 
-        database.Database() context manager. Review documentation of Database 
-        Module for more information.
-    
-    3. Initialize the builder by assigning it to a variable and passing any
-        information required by its __init__ method.
+        fentanyl = med_builder.build()
+        ```
 
-    4. Call the various 'set' methods and pass in the required information.
+    Review the documentation of specific builders for more information on 
+    their usage and available methods.
 
-    5. Call the builder's `build()` method and assign it to a variable.
-
-Example:
-
-    ```python
-    from narcotics_tracker.builders import medication_builder
-
-    med_builder = medication_builder.MedicationBuilder()
-
-    med_builder.set_medication_id(None)
-    med_builder.set_name("Aspirin")
-    med_builder.set_code("ASA")
-    med_builder.set_fill_amount(10)
-    med_builder.set_container(containers.Container.AMPULE)
-    med_builder.set_dose_and_unit(10, units.Unit.MCG)
-    med_builder.set_status(medication_statuses.MedicationStatus.ACTIVE)
-    med_builder.set_created_date(None)
-    med_builder.set_modified_date(None)
-    med_builder.set_modified_by("SRK")
-
-    aspirin = med_builder.build()
-    ```
- """
+#* Modules:
+    Builder Interface: Contains the interface for concrete DataItem builders.
+    Adjustment Builder: Contains the concrete builder for Adjustment DataItems.
+    Event Builder: Contains the concrete builder for the Event DataItems.
+    Medication Builder: Contains the concrete builder for Medication DataItems.
+    Reporting Period Builder: Contains the concrete builder for ReportingPeriod 
+        DataItems.
+    Status Builder: Contains the concrete builder for the Status DataItems.
+    Unit Builder: Contains the concrete builder for the Unit DataItems.
+"""
