ec677a6a9917ef41bb244fefe2462ed4f389edf3
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-10-25 20:03:20 -0400

    Add AddAdjustment Command

--- narcotics_tracker/commands/adjustment_commands.py
+++ narcotics_tracker/commands/adjustment_commands.py
@@ -2,63 +2,77 @@
 
 Please see the package documentation for more information.
 """
-from typing import TYPE_CHECKING
 
 from narcotics_tracker.commands.command_interface import SQLiteCommand
-from narcotics_tracker.database import SQLiteManager
-
-if TYPE_CHECKING:
-    from narcotics_tracker.items.adjustments import Adjustment
+from narcotics_tracker.items.adjustments import Adjustment
+from narcotics_tracker.persistence_interface import PersistenceManager
 
 
 class AddAdjustment(SQLiteCommand):
-    """Adds and Adjustment to the database.
+    """Adds an Adjustment to the database.
 
-    Attributes:
-        receiver: Persistence manager for the data repository.
+    Methods:
+        execute: Executes the command, returns success message."""
 
-    """
+    def __init__(self, receiver: PersistenceManager, adjustment: Adjustment) -> None:
+        """Initializes the command. Sets the receiver and adjustment.
 
-    def __init__(self, receiver: SQLiteManager, item: Adjustment) -> None:
-        self._target = receiver
-        self._dataitem = item
+        Args:
+            receiver (PersistenceManager): Persistence manager for the data
+                repository.
+
+            adjustment: That adjustment to be added to the database.
+        """
+        self._receiver = receiver
+        self._adjustment = adjustment
 
     def execute(self) -> str:
         """Executes the command, returns success message."""
 
-        self._extract_item_info()
+        self._extract_adjustment_info()
         table_name = self._pop_table_name()
 
-        self._target.add(table_name, self.item_info)
+        self._receiver.add(table_name, self.adjustment_info)
 
-        return f"Item added to {table_name} table."
+        return f"adjustment added to {table_name} table."
 
-    def _extract_item_info(self) -> None:
-        """Extracts item attributes and stored as a dictionary."""
-        self.item_info = vars(self._dataitem)
+    def _extract_adjustment_info(self) -> None:
+        """Extracts adjustment attributes and stored as a dictionary."""
+        self.adjustment_info = vars(self._adjustment)
 
     def _pop_table_name(self) -> str:
-        """Removes and returns the table name from DataItem information.
+        """Removes and returns the table name from Adjustment's attributes.
 
         Returns:
             string: Name of the table.
         """
-        return self.item_info.pop("table")
+        return self.adjustment_info.pop("table")
 
 
 class DeleteAdjustment(SQLiteCommand):
-    """Deletes an Adjustment from the database by its ID."""
+    """Deletes an Adjustment from the database by its ID.
 
-    def __init__(self, receiver: SQLiteManager, adjustment_id: int) -> None:
-        """Sets the SQLiteManager and adjustment_id."""
-        self._target = receiver
-        self._dataitem_id = adjustment_id
+    Methods:
+        execute: Executes the delete operation and returns a success message.
+    """
+
+    def __init__(self, receiver: PersistenceManager, adjustment_id: int) -> None:
+        """Initializes the command. Sets the receiver and adjustment.
+
+        Args:
+            receiver (PersistenceManager): Persistence manager for the data
+                repository.
+
+            adjustment: That adjustment to be added to the database.
+        """
+        self._receiver = receiver
+        self._adjustment_id = adjustment_id
 
     def execute(self) -> str:
         """Execute the delete operation and returns a success message."""
-        self._target.remove("inventory", {"id": self._dataitem_id})
+        self._receiver.remove("inventory", {"id": self._adjustment_id})
 
-        return f"Adjustment #{self._dataitem_id} deleted."
+        return f"Adjustment #{self._adjustment_id} deleted."
 
 
 class ListAdjustments(SQLiteCommand):
@@ -66,28 +80,28 @@ class ListAdjustments(SQLiteCommand):
 
     def __init__(
         self,
-        receiver: SQLiteManager,
+        receiver: PersistenceManager,
         criteria: dict[str] = {},
         order_by: str = None,
     ):
-        """Sets the SQLiteManager, criteria and order_by column.
+        """Sets the command's target, criteria and order_by column.
 
         Args:
-            receiver (SQLiteManager): SQLiteManager connected to the database.
+            receiver (PersistenceManager): PersistenceManager connected to the database.
 
             criteria (dict[str, any], optional): Criteria of adjustments to be
                 returned as a dictionary mapping column names to values.
 
             order_by (str, optional): Column name by which to sort the results.
         """
-        self._target = receiver
+        self._receiver = receiver
         self._criteria = criteria
         self._order_by = order_by
 
     def execute(self) -> list[tuple]:
         """Executes the command and returns a list of Adjustments."""
 
-        cursor = self._target.read("inventory", self._criteria, self._order_by)
+        cursor = self._receiver.read("inventory", self._criteria, self._order_by)
         return cursor.fetchall()
 
 
@@ -95,15 +109,27 @@ class UpdateAdjustment(SQLiteCommand):
     """Update an Adjustment with the given data and criteria."""
 
     def __init__(
-        self, receiver: SQLiteManager, data: dict[str, any], criteria: dict[str, any]
+        self,
+        receiver: PersistenceManager,
+        data: dict[str, any],
+        criteria: dict[str, any],
     ) -> None:
-        """Sets the SQLiteManager, updates data, and selection criteria."""
-        self._target = receiver
+        """Sets the command's target, criteria and order_by column.
+
+        Args:
+            receiver (PersistenceManager): PersistenceManager connected to the database.
+
+            criteria (dict[str, any], optional): Criteria of adjustments to be
+                returned as a dictionary mapping column names to values.
+
+            order_by (str, optional): Column name by which to sort the results.
+        """
+        self._receiver = receiver
         self._data = data
         self._criteria = criteria
 
     def execute(self) -> str:
         """Executes the update operation and returns a success message."""
-        self._target.update("inventory", self._data, self._criteria)
+        self._receiver.update("inventory", self._data, self._criteria)
 
         return f"Adjustment data updated."
--- tests/Integration/dataItem_storage_test.py
+++ tests/Integration/dataItem_storage_test.py
@@ -20,15 +20,10 @@ Functions:
 
 
 import sqlite3
-from typing import TYPE_CHECKING
 
 from narcotics_tracker import commands
-from narcotics_tracker.builders.adjustment_builder import AdjustmentBuilder
 from narcotics_tracker.database import SQLiteManager
 from narcotics_tracker.utils.datetime_manager import DateTimeManager
-
-if TYPE_CHECKING:
-    from narcotics_tracker.items.adjustments import Adjustment
 
 
 def return_ids(cursor: sqlite3.Cursor) -> list[int]:
