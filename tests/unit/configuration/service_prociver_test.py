5c632ea3bc5b4a55e02b75dd4d2a19f6cef38b0a
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-10-26 02:03:05 -0400

    Add tests for ServiceProvider

--- tests/unit/configuration/service_prociver_test.py
+++ tests/unit/configuration/service_prociver_test.py
@@ -21,10 +21,46 @@ class Test_ServiceProvider:
     """
 
     def test_ServiceProvider_returns_persistence_service(self) -> None:
-        assert isinstance(ServiceProvider().start_services()[0], PersistenceService)
+        service_provider = ServiceProvider()
+        db, dt, converter = service_provider.start_services()
+
+        assert isinstance(db, PersistenceService)
 
     def test_ServiceProvider_returns_datetime_service(self) -> None:
-        assert isinstance(ServiceProvider().start_services()[1], DateTimeService)
+        service_provider = ServiceProvider()
+        db, dt, converter = service_provider.start_services()
+
+        assert isinstance(dt, DateTimeService)
 
     def test_ServiceProvider_returns_conversion_service(self) -> None:
-        assert isinstance(ServiceProvider().start_services()[2], ConversionService)
+        service_provider = ServiceProvider()
+        db, dt, converter = service_provider.start_services()
+
+        assert isinstance(converter, ConversionService)
+
+    def test_persistence_service_returns_expected_results(self, reset_database) -> None:
+        service_provider = ServiceProvider()
+        db, dt, converter = service_provider.start_services()
+
+        db.create_table("test_table", {"data": "TEXT"})
+
+        db.add("test_table", {"data": "Hello"})
+
+        cursor = db.read("test_table")
+        data = cursor.fetchall()[0][0]
+
+        assert data == "Hello"
+
+    def test_datetime_service_returns_expected_result(self) -> None:
+        service_provider = ServiceProvider()
+        db, dt, converter = service_provider.start_services()
+
+        assert dt.convert_to_timestamp("01-02-1986 14:10:00") == 505077000
+
+    def test_conversion_service_returns_expected_results(self) -> None:
+        service_provider = ServiceProvider()
+        db, dt, converter = service_provider.start_services()
+
+        answer = converter.to_standard(1, "mg")
+
+        assert answer == 1000
