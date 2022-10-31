c9393e8a9f2d6a4df4e3414a020a7fc130339ec0
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-10-31 15:12:05 -0400

    Refactor and simplify script

--- narcotics_tracker/scripts/wlvac_adjustment.py
+++ narcotics_tracker/scripts/wlvac_adjustment.py
@@ -1,127 +1,88 @@
 """Adds all inventory adjustments to the WLVAC Inventory."""
 
+from typing import TYPE_CHECKING
+
 from narcotics_tracker import commands
 from narcotics_tracker.builders.adjustment_builder import AdjustmentBuilder
-from narcotics_tracker.items.adjustments import Adjustment
-from narcotics_tracker.services.datetime_manager import DateTimeManager
 from narcotics_tracker.services.service_manager import ServiceManager
-from narcotics_tracker.services.sqlite_manager import SQLiteManager
 
-dt_man = DateTimeManager()
-sq_man = SQLiteManager("inventory_wlvac.db")
+if TYPE_CHECKING:
+    from narcotics_tracker.items.adjustments import Adjustment
+
+
+def main() -> None:
+
+    adjustment_data = return_adjustments_data()
+
+    adjustment_list = construct_adjustments(adjustment_data)
+
+    for adjustment in adjustment_list:
+        message = commands.AddAdjustment().execute(adjustment)
+        print(message)
 
 
 def construct_adjustments(data: list[any]) -> list["Adjustment"]:
     adjustment_list = []
     for data_set in data:
-        adjustment = Adjustment(
-            table="inventory",
-            id=data_set[0],
-            created_date=dt_man.return_current(),
-            modified_date=dt_man.return_current(),
-            modified_by="SRK",
-            adjustment_date=data_set[1],
-            event_code=data_set[2],
-            medication_code=data_set[3],
-            amount=data_set[4],
-            reference_id=data_set[6],
-            reporting_period_id=data_set[5],
+        adjustment = (
+            AdjustmentBuilder()
+            .set_table("inventory")
+            .set_id(data_set[0])
+            .set_created_date()
+            .set_modified_date()
+            .set_modified_by("SRK")
+            .set_adjustment_date(data_set[1])
+            .set_event_code(data_set[2])
+            .set_medication_code(data_set[3])
+            .set_adjustment_amount(data_set[4])
+            .set_reporting_period_id(data_set[5])
+            .set_reference_id(data_set[5])
+            .build()
         )
         adjustment_list.append(adjustment)
 
     return adjustment_list
 
 
-adjustment = Adjustment(
-    table="inventory",
-    id=None,
-    created_date=dt_man.return_current(),
-    modified_date=dt_man.return_current(),
-    modified_by="SRK",
-    adjustment_date=dt_man.convert_to_timestamp("07-22-2022 17:00:00"),
-    event_code="IMPORT",
-    medication_code="fentanyl",
-    amount=7450,
-    reference_id="Narcotics Bi-Annual Report - June 2022 - Wantagh-Levittown VAC",
-    reporting_period_id=2200000,
-)
-
-midazolam_june_2022 = Adjustment(
-    table="inventory",
-    id=None,
-    created_date=dt_man.return_current(),
-    modified_date=dt_man.return_current(),
-    modified_by="SRK",
-    adjustment_date=dt_man.convert_to_timestamp("07-22-2022 17:00:00"),
-    event_code="IMPORT",
-    medication_code="midazolam",
-    amount=265360.0,
-    reference_id="Narcotics Bi-Annual Report - June 2022 - Wantagh-Levittown VAC",
-    reporting_period_id=2200000,
-)
-
-morphine_june_2022 = Adjustment(
-    table="inventory",
-    id=None,
-    created_date=dt_man.return_current(),
-    modified_date=dt_man.return_current(),
-    modified_by="SRK",
-    adjustment_date=dt_man.convert_to_timestamp("07-22-2022 17:00:00"),
-    event_code="IMPORT",
-    medication_code="morphine",
-    amount=690000,
-    reference_id="Narcotics Bi-Annual Report - June 2022 - Wantagh-Levittown VAC",
-    reporting_period_id=2200000,
-)
-
-
-def main() -> None:
-
-    service_provider = ServiceManager(repository="inventory_wlvac.db")
-    sq, dt, converter = service_provider.start_services()
-
-    adjustment_data = []
-
-    use_1 = [None, 1659212760, "USE", "fentanyl", -50, 2200000, "PCR# 220830"]
-    adjustment_data.append(use_1)
-    des_1 = [
-        None,
-        1661027838,
-        "DESTRUCTION",
-        "morphine",
-        -440000,
-        2200000,
-        "RxRD# 37265",
+def return_adjustments_data() -> list[list]:
+    return [
+        [
+            None,
+            "07-22-2022 17:00:00",
+            "IMPORT",
+            "fentanyl",
+            745,
+            2200001,
+            "Narcotics Bi-Annual Report - June 2022 - Wantagh-Levittown VAC",
+        ],
+        [
+            None,
+            "07-22-2022 17:00:00",
+            "IMPORT",
+            "midazolam",
+            663.4,
+            2200001,
+            "Narcotics Bi-Annual Report - June 2022 - Wantagh-Levittown VAC",
+        ],
+        [
+            None,
+            "07-22-2022 17:00:00",
+            "IMPORT",
+            "morphine",
+            690,
+            2200001,
+            "Narcotics Bi-Annual Report - June 2022 - Wantagh-Levittown VAC",
+        ],
+        [None, 1659212760, "USE", "fentanyl", -50, 2200001, "PCR# 220830"],
+        [None, 1661027838, "DESTRUCTION", "morphine", -440, 2200001, "RxRD# 37265"],
+        [None, 1661027838, "DESTRUCTION", "midazolam", -363.4, 2200001, "RxRD# 37265"],
+        [None, 1661027838, "DESTRUCTION", "fentanyl", -345, 2200001, "RxRD# 37265"],
+        [None, 1661166388, "USE", "midazolam", -5, 2200001, "PCR# 220920"],
+        [None, 1661701387, "USE", "fentanyl", -60, 2200001, "PCR# 220945"],
+        [None, 1662580020, "USE", "fentanyl", -100, 2200001, "PCR# 220976"],
+        [None, 1665258240, "USE", "midazolam", -5, 2200001, "PCR# 221095"],
+        [None, 1666487700, "USE", "midazolam", -1.6, 2200001, "PCR# 221144"],
     ]
-    adjustment_data.append(des_1)
-    des_2 = [
-        None,
-        1661027838,
-        "DESTRUCTION",
-        "midazolam",
-        -363400,
-        2200000,
-        "RxRD# 37265",
-    ]
-    adjustment_data.append(des_2)
-    des_3 = [None, 1661027838, "DESTRUCTION", "fentanyl", -3450, 2200000, "RxRD# 37265"]
-    adjustment_data.append(des_3)
-    use_2 = [None, 1661166388, "USE", "midazolam", -5000, 2200000, "PCR# 220920"]
-    adjustment_data.append(use_2)
-    use_3 = [None, 1661701387, "USE", "fentanyl", -60, 2200000, "PCR# 220945"]
-    adjustment_data.append(use_3)
-    use_4 = [None, 1662580020, "USE", "fentanyl", -100, 2200000, "PCR# 220976"]
-    adjustment_data.append(use_4)
-    use_5 = [None, 1665258240, "USE", "midazolam", -5000, 2200000, "PCR# 221095"]
-    adjustment_data.append(use_5)
-    use_6 = [None, 1666487700, "USE", "midazolam", -1600, 2200000, "PCR# 221144"]
-    adjustment_data.append(use_6)
-
-    adjustment_list = construct_adjustments(adjustment_data)
-
-    for adjustment in adjustment_list:
-        message = commands.AddAdjustment(sq, adjustment).execute()
-        print(message)
 
 
 if __name__ == "__main__":
