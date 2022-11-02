f06124f83c4d6ef2b995059e4f0fb0477d86df9e
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-11-02 01:46:18 -0400

    Update documentation

--- narcotics_tracker/reports/__init__.py
+++ narcotics_tracker/reports/__init__.py
@@ -1,1 +1,12 @@
-"""Contains the modules required to return reports regarding the inventory."""
+"""Contains the modules required to return reports regarding the inventory.
+
+Reports:
+    ReturnCurrentInventory: Returns the current stock for all active 
+        medications in the inventory.
+    
+    ReturnMedicationStock: Returns the current amount on hand for a specific 
+        medication.
+"""
+
+from narcotics_tracker.reports.return_current_inventory import ReturnCurrentInventory
+from narcotics_tracker.reports.return_medication_stock import ReturnMedicationStock
--- tests/Integration/reports/__init__.py
+++ tests/Integration/reports/__init__.py
@@ -1,1 +1,9 @@
-"""Contains the integration tests for the Reports Package."""
+"""Contains the integration tests for the Reports Package.
+
+Modules:
+    return_current_inventory_test: Handles integration testing of the 
+        ReturnCurrentInventory Report.
+    
+    return_medication_stock_test: Contains the classes which test the 
+        ReturnMedicationStock Report.
+    """
