b41b8dbe427b299d31d948d1530bf7233d97ec1e
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-11-02 01:27:07 -0400

    Update with tests

--- tests/Integration/reports/return_medication_stock_test.py
+++ tests/Integration/reports/return_medication_stock_test.py
@@ -1,15 +1,19 @@
-"""Contains the classes which test the ReturnMedicationStock Report."""
+"""Contains the classes which test the ReturnMedicationStock Report.
+
+Classes:
+    Test_ReturnMedicationStock: Integration tests the ReturnMedicationStock 
+        Report.
+"""
 
 from narcotics_tracker.reports.return_medication_stock import ReturnMedicationStock
 from narcotics_tracker.services.sqlite_manager import SQLiteManager
-from tests import conftest
 
 
 class Test_ReturnMedicationStock:
     """Integration tests the ReturnMedicationStock Report.
 
     Behaviors Tested:
-        - Returns correct restuls from SQLite3 database.
+        - Returns correct results from SQLite3 database.
     """
 
     def test_report_returns_correct_results(self, setup_integration_db):
