2b8ce7551281a924706786b429b14eabdff09a2c
ScottSucks <89794691+ScottSucksAtPython@users.noreply.github.com>
2022-11-26 23:26:19 -0500

    Fix typing error

--- narcotics_tracker/commands/interfaces/command.py
+++ narcotics_tracker/commands/interfaces/command.py
@@ -1,10 +1,13 @@
 """Defines the protocol for commands which interact with the SQLite3 database.
 
-Classes: 
+Classes:
     SQLiteCommand: The interface for SQLite3 database commands.
 
 """
-from typing import Protocol
+from typing import TYPE_CHECKING, Protocol
+
+if TYPE_CHECKING:
+    from narcotics_tracker.services.interfaces.persistence import PersistenceService
 
 
 class Command(Protocol):
@@ -16,10 +19,8 @@ class Command(Protocol):
         def execute(self) -> None:
     """
 
-    def __init__(self, receiver) -> None:
+    def __init__(self, receiver: "PersistenceService") -> None:
         """Sets the receiver of the command."""
-        ...
 
     def execute(self) -> None:
         """Executes the command. Accepts parameters required by the receiver."""
-        ...
