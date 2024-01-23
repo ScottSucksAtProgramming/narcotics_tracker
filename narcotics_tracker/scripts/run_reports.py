2dcd7d93b21b93fe09595c2c990617a162d91d32
ScottSucksAtProgramming <scottkostolni+github@gmail.com>
2024-01-23 13:59:46 -0500

    Updates Docstrings

--- narcotics_tracker/scripts/run_reports.py
+++ narcotics_tracker/scripts/run_reports.py
@@ -4,10 +4,14 @@ import os
 from typing import Any
 
 from narcotics_tracker import reports
+from narcotics_tracker.services.sqlite_manager import SQLiteManager
 
 
 def main():
-    """Main Function."""
+    """
+    The main function clears the console, retrieves the current inventory using reports.ReturnCurrentInventory().run(),
+    converts the inventory data to strings, and then prints the Wantagh-Levittown VAC Narcotics Inventory.
+    """
     os.system("clear")
     totals = reports.ReturnCurrentInventory().run()
 
@@ -20,6 +24,12 @@ def main():
 
 
 def _make_strings(db_totals: list[dict[str, Any]]) -> list[str]:
+    """
+    Generate a list of strings based on the input list of dictionaries.
+
+    :param db_totals: A list of dictionaries containing 'name', 'amount', and 'unit' keys.
+    :return: A list of strings created from the input dictionaries.
+    """
     strings: list[str] = []
 
     for item in db_totals:
